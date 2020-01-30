bed6c6e8e85f95fb58c79ff70789022f277f73d2
Achyut <adeshpande@carbonite.com>
2020-01-30 10:04:30 +0530

    fix

--- testempty.groovy
+++ testempty.groovy
@@ -1,5 +1,10 @@
 #!groovy
 import java.lang.String
+import hudson.model.*
+import hudson.AbortException
+import hudson.console.HyperlinkNote
+import java.util.concurrent.CancellationException
+ 
 
 class Demo {                                  
     String branch = 'develop'
@@ -8,9 +13,14 @@ class Demo {
     
     def exec() {        
         final String buildSubJob = 'job3'
-        def buildResult = build job: buildSubJob,
-            propagate: false
-
+        //def buildResult = build job: buildSubJob,
+        //    propagate: false
+		def job = hudson.model.Hudson.instance.getJob("job3")
+def params = new StringParameterValue('PARAMTEST', "somestring")  
+def paramsAction = new ParametersAction(params) 
+def cause = new hudson.model.Cause.UpstreamCause(currentBuild)
+def causeAction = new hudson.model.CauseAction(cause) 
+hudson.model.Hudson.instance.queue.schedule(job, 0, causeAction, paramsAction) 
         println "** Completed ***"
     }
 	
@@ -20,7 +30,7 @@ class Demo {
     println "Hello Achyut."
 	final String buildSubJob = 'job3'
 	def buildResult = build job: buildSubJob,
-    propagate: false
+		propagate: false
 }
 }
 
@@ -30,4 +40,5 @@ class Demo {
 // d.exec()
 
 //return new Demo()
-return new Demo()
+def obj = new Demo()
+obj.exec()
