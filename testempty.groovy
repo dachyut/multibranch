398c43418f28d0f0062d430cfcaa4b22e9e56d54
Achyut <adeshpande@carbonite.com>
2020-01-30 08:11:36 +0530

    fix

---
+++ testempty.groovy
@@ -0,0 +1,24 @@
+#!groovy
+import java.lang.String
+
+class Demo {                                  
+    String branch = 'develop'
+    String build = 'FAST'
+    String commit = "new"
+    
+    def exec() {        
+        final String buildSubJob = 'job3'
+        def buildResult = build job: buildSubJob,
+            propagate: false
+
+        println "** Completed ***"
+    }
+}
+
+// Demo d = new Demo()
+// d.branch = "jhf"
+// d.build = "dsfsdfsdf"
+// d.exec()
+
+//return new Demo()
+return this
