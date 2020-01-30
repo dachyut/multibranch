96a6ca24ec7c7951a4ec22d9d1f1b3b4cf15095e
Achyut <adeshpande@carbonite.com>
2020-01-30 10:22:28 +0530

    fix

--- testempty.groovy
+++ testempty.groovy
@@ -17,9 +17,7 @@ class Demo {
 		def job = Hudson.instance.getJob('job3')
 		def anotherBuild
 		try {
-			def params = [
-			  new StringParameterValue('FOO', foo),
-			]
+			
 			def future = job.scheduleBuild2(0, new Cause.UpstreamCause(build), new ParametersAction(params))
 			println "Waiting for the completion of " + HyperlinkNote.encodeTo('/' + job.url, job.fullDisplayName)
 			anotherBuild = future.get()
