5b85f37bd9ac14bc46a5e5e4574d0491074e50c5
Achyut <adeshpande@carbonite.com>
2020-02-03 11:11:52 +0530

    fix

--- test2.groovy
+++ test2.groovy
@@ -1,11 +1,22 @@
 #!groovy
 import java.lang.String
 
-class Demo {
+def out = new Binding()
+
+class Demo extends Script {
 	String branchOrCommit='Default'	
 	String buildType='FAST'
-	
-	def execprint() {
+    
+    //def out = new Binding()	
+    Demo(out)  // Have to pass the out variable to the class
+    {
+        script.echo("Hello-AAAAAA")
+        out.println ("Inside class OutoutClass")
+    }
+
+	def execprint(out) {
+        script.echo("Hello-BBBBBBBBB")
+        out.println ("Inside class exec")
 		//def buildit = load 'test1.groovy'			
 		GroovyShell shell = new GroovyShell()
 	    def tools = shell.parse(new File('test1.groovy'))
