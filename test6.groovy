3670696cdb8131bd6b6fd26687339391a395b862
Achyut <adeshpande@carbonite.com>
2020-02-05 15:14:40 +0530

    fix

--- test6.groovy
+++ test6.groovy
@@ -1,18 +1,18 @@
 class MyClass {
 
     def exec() {
-        println "Inside exec method"
+        echo "Inside exec method"
         def name = "PipelineJob2"
-        def archiveName = 'build.properties'
+        //def archiveName = 'build.properties'
         try {
-            step($class: 'hudson.plugins.copyartifact.CopyArtifact',  name, selector: lastSuccessful(), filter: "*", fingerprintArtifacts: true)
+            step($class: 'hudson.plugins.copyartifact.CopyArtifact', projectName: name, selector: lastSuccessful(), fingerprintArtifacts: true)
         } catch (none) {
-            echo 'No artifact to copy from ' + name + ' with name ' + archiveName
-            writeFile file: archiveName, text: '3'
+            echo 'No artifact to copy from ' + name 
+            //writeFile file: archiveName, text: '3'
         }
 
-        def content = readFile(archiveName).trim()
-        echo 'value archived: ' + content
+        //def content = readFile(archiveName).trim()
+        echo 'value archived: '
     }
 }
 
