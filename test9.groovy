00fa6a3aa551192cc7aad82fcebcbf8c60f7b060
Achyut <adeshpande@carbonite.com>
2020-02-06 16:16:00 +0530

    fix

--- test9.groovy
+++ test9.groovy
@@ -9,7 +9,7 @@ def exec() {
     def j1EnvVariables = job1_props.getBuildVariables();
     println "${j1EnvVariables["BUILD_ID"]}" 
     println "${j1EnvVariables["BUILD_ID"].getClass()}" 
-    def n = hudson.plugins.copyartifact.BuildSelector(j1EnvVariables["BUILD_ID"].getClass())
+    def n = BuildSelector(j1EnvVariables["BUILD_ID"].getClass())
 }
 
 return this
