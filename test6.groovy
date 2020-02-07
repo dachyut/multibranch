45075f48a92c4cfe629c5112919937ac91b1fcfc
Achyut <adeshpande@carbonite.com>
2020-02-07 15:16:16 +0530

    fix

--- test6.groovy
+++ test6.groovy
@@ -1,42 +1,22 @@
 #!groovy
-//import java.lang.String
-//import io.jenkins.plugins.analysis.core.*
-//import io.jenkins.plugins.analysis.core.steps.*
+import java.lang.String
+import io.jenkins.plugins.analysis.core.*
+import io.jenkins.plugins.analysis.core.steps.*
 import io.jenkins.plugins.analysis.core.model.*
 
-class absClass extends Tool { 
-    String id
-    // def absClass() {
-    //     super()
-    // }
-    public void setId(String name) {
-        id = name
-    }
-
-    public String getId() {
-        script.println("ID:------ ${id}")
-        return id
-    }
-}
-
-class MyClass {
-    Script script;
-    
-    script.absClass ob = new script.absClass()
+class MyClass extends absClass {
+    Script script;       
     
     def exec() {
         script.println "Inside test6 exec method"
         final String buildLog = 'build.log, **/build.log'
-
-        ob.id = "123"
-        ob.setID("1234")
-        script.println(ob.getID())
+    
             
         //recordIssues(tools: [msBuild(pattern: buildLog)])
 
         //def t = steps.RecordIssuesStep.setTools('msbuild')
         //script.println "${t}"
-        //script.recordIssues (tool : "msbuild")
+        script.recordIssues (tools : "msbuild")
         //script.recordIssues(enabledForFailure: true, aggregatingResults: true,
         //    tools: t)
         //.msBuild(pattern: buildLog)
