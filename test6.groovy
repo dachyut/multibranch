594bf46741833f375eec2db61c709a91cb14bf59
Achyut <adeshpande@carbonite.com>
2020-02-07 16:50:08 +0530

    fix

--- test6.groovy
+++ test6.groovy
@@ -11,7 +11,7 @@ class MyClass {
         script.println "Inside test6 exec method"
         final String buildLog = 'build.log, **/build.log'
     
-        def t = script.tool(name: 'msbuild', type: 'io.jenkins.plugins.analysis.core.model.Tool')
+        //def t = script.tool(name: 'msbuild', type: 'io.jenkins.plugins.analysis.core.model.Tool')
         //recordIssues(tools: [msBuild(pattern: buildLog)])
 
         //def t = steps.RecordIssuesStep.setTools('msbuild')
