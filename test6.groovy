dc736446057621382292c5b2ec2ca68b208d67e4
Achyut <adeshpande@carbonite.com>
2020-02-07 12:56:45 +0530

    fix

--- test6.groovy
+++ test6.groovy
@@ -10,7 +10,7 @@ class MyClass {
         final String buildLog = 'build.log, **/build.log'
         
         //recordIssues(tools: [msBuild(pattern: buildLog)])
-        script.recordIssues(enabledForFailure: true, aggregatingResults: true, tool: this.msbuild)
+        script.recordIssues(enabledForFailure: true, aggregatingResults: true, tools: msbuild)
         //.msBuild(pattern: buildLog)
         //script.recordIssues(tools: msbld)
     }
