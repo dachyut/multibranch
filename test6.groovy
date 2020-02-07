2e0837a9ca710765d100f6680cb117b50e107a71
Achyut <adeshpande@carbonite.com>
2020-02-07 12:11:12 +0530

    fix

--- test6.groovy
+++ test6.groovy
@@ -6,38 +6,10 @@ class MyClass {
     Script script;
 
     def exec() {
-        ////echo "Inside exec method"
-        def name = "PipelineJob2"
-        def archiveName = 'build.properties'
-        //try {
-        script.echo("CopyArtifacts.........")
+        script.println "Inside test6 exec method"
+        final String buildLog = 'build.log, **/build.log'
         
-        def n = script.specific("6")
-        script.println "n: ${n}"
-        script.println "n class: ${n.getClass()}"
-        
-        script.copyArtifacts(
-            projectName: name,
-            filter: 'build.properties',
-            fingerprintArtifacts: true,
-            selector : n,
-            flatten: true
-        )
-        script.println("Archive artifacts")
-        script.archiveArtifacts(artifacts: archiveName, fingerprint: true)
-        //} catch (none) {
-            ////echo 'No artifact to copy from ' + name 
-            //writeFile file: archiveName, text: '3'
-        //}
-        script.println("Hellooooooooooo")
-        //script.echo("Listing files......")
-        // def rootFiles = new File("test").listRoots() 
-        // rootFiles.each { file -> script.echo(file.absolutePath) }
-
-        // script.echo("File contents.......")
-        // File file = new File(archiveName)
-        // def lines = file.text
-        // script.echo("File : ${archiveName}")        
+        script.recordIssues(tools: [msBuild(pattern: buildLog)])
     }
 }
 
