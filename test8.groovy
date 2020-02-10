e8959cbe87dbfc81fdf692202320ea46ccc18c77
Achyut <adeshpande@carbonite.com>
2020-02-10 12:08:21 +0530

    fix

--- test8.groovy
+++ test8.groovy
@@ -83,7 +83,7 @@ class MyClass {
         script.archiveArtifacts(artifacts: buildArtifacts, fingerprint: true)
         //script.warningsParsers(parserName: 'MSBuild', pattern: buildLog)
         //io.jenkins.plugins.analysis.core.model.Tool.
-        def d = script.io.jenkins.plugins.analysis.core.model.Tool.getDescriptor() 
+        def d = script.getDescriptor() 
         script.println (d)
 
 
