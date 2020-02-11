a8f9732a9d58e7777ecad83d0137ad483fad7312
Achyut <adeshpande@carbonite.com>
2020-02-11 16:07:52 +0530

    fix

--- test8.groovy
+++ test8.groovy
@@ -87,7 +87,7 @@ class MyClass {
         //Below this are testing scripts
         script.println("Testing code...")
         
-        script.recordIssues(tools: msBuild(pattern: 'build.log'))
+        script.recordIssues(tools: msBuild[[(pattern: 'build.log')]])
 
     } //exec
 
