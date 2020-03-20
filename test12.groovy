3f56dfa7b40d0d14c0544049ef29af5cca5ba9a9
Achyut <adeshpande@carbonite.com>
2020-03-20 14:47:32 +0530

    fix

--- test12.groovy
+++ test12.groovy
@@ -11,12 +11,12 @@ class BuildTimer {
         // Given a slash-delimited string, such as Jenkins env.JOB_NAME,
         // reformat to comma-delimited string for CSV output
         script.println "In printJobScopes()"  
-        stash includes: "buildProperties", name: 'properties'          
+        script.stash includes: "buildProperties", name: 'properties'          
     }
 
     def recordHeaders() {
         script.println "In recordHeaders()"
-        
+
     }
 
     // void finalize() { 
