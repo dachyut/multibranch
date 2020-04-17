81b20d40b090fa10dad6d60debf174f793d5ae78
Achyut <adeshpande@carbonite.com>
2020-04-17 12:54:10 +0530

    fix

--- test15.groovy
+++ test15.groovy
@@ -6,11 +6,11 @@ class MyClass {
     
     def exec() {
         script.println "Inside test15 exec method"
-        String AZURE_SERVICE_PRINCIPAL_KEY = 'sss'
+        //String AZURE_SERVICE_PRINCIPAL_KEY = 'sss'
         script.withCredentials([script.string(credentialsId: 'jira_passws', variable: 'AZURE_SERVICE_PRINCIPAL_KEY')]) {
             script.println "Hello !!!!!!!!!!!!!!!!!"
             
-            script.println "${AZURE_SERVICE_PRINCIPAL_KEY}"
+            script.println "$AZURE_SERVICE_PRINCIPAL_KEY"
         }
     }
 }
